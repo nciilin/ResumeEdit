09ec57f8f822260e854a97ec7490915f381dbfd2
vincentcheng <670700452@qq.com>
2019-08-28 20:14:12 +0800

    修复dist上线

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/ResumeEdit/dist',
+    assetsPublicPath: '/ResumeEdit/dist/',
 
     /**
      * Source Maps
